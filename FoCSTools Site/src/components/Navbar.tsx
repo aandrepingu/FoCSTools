aa9c81a0f4a23c1e1fe13435e8cf0a352a392189
Enigma-iwnl <108947845+Enigma-iwnl@users.noreply.github.com>
2023-09-22 16:03:49 -0400

    idk what this is

--- FoCSTools Site/src/components/Navbar.tsx
+++ FoCSTools Site/src/components/Navbar.tsx
@@ -4,8 +4,14 @@ export default function Navbar() {
   return (
     <>
       <div className="navbar">
-        <div className="flex" style={{ paddingLeft: "20px" }}>
-          LOGO
+        <div
+          className="grid"
+          style={{ paddingLeft: "20px", gridTemplateColumns: "1fr 1fr" }}
+        >
+          <div className="flex">LOGO</div>
+          <div className="flex" style={{ justifyContent: "center" }}>
+            TITLE
+          </div>
         </div>
         <div className="flex" style={{ justifyContent: "center" }}>
           <button>1</button>
