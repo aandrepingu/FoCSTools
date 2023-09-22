1f11b66778e4074519c80e19e88abb893aa86dd2
faizaan5ali <130408625+faizaan5ali@users.noreply.github.com>
2023-09-22 17:42:03 -0400

    Basic Sidebar implementation
    Sidebar test

--- FoCSTools Site/src/App.tsx
+++ FoCSTools Site/src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 export default function App() {
   return (
-    <>
-      <Navbar />
-    </>
+    <div>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Sidebar />
+      </div>
+    </div>
   );
 }
---
+++ FoCSTools Site/src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import "./Sidebar.css";
+
+export default function Sidebar() {
+    return (
+      <>
+        <div className="sidebar" style={{paddingLeft: "0px"}}>
+          
+        </div>
+      </>
+    );
+  }
