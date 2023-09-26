81fd920d3e1d6d4da20d23bc48118557de45b247
Enigma-iwnl <108947845+Enigma-iwnl@users.noreply.github.com>
2023-09-26 17:41:11 -0400

    Revert "test"
    This reverts commit cda2c23b2ce41a07b349060a66272498551bcea3, reversing
    changes made to 480e148a6a4f4dcaedf7761cef4de94adaaaf91c.

--- FoCSTools Site/src/pages/DFA.tsx
+++
@@ -1,7 +0,0 @@
-export default function DFA() {
-  return (
-    <>
-      <div style={{ height: "50%", width: "50%" }}>DFA Page</div>
-    </>
-  );
-}
--- FoCSTools Site/src/pages/Test.tsx
+++
@@ -1,9 +0,0 @@
-//import Navbar from "../components/Navbar";
-
-export default function Test() {
-  return (
-    <>
-      <div style={{ height: "50%", width: "50%" }}>This is a Test</div>
-    </>
-  );
-}
