f4a8420b4784c653632bcd4bcabf6471c95da155
Enigma-iwnl <108947845+Enigma-iwnl@users.noreply.github.com>
2023-09-26 16:41:58 -0400

    logo done, metadata updated

--- FoCSTools Site/src/components/Navbar.tsx
+++ FoCSTools Site/src/components/Navbar.tsx
@@ -4,9 +4,7 @@ export default function Navbar() {
   return (
     <>
       <div className="navbar">
-        <div style={{ width: "30px", height: "30px" }}>
-          <img src="logo.svg" className="logo" />
-        </div>
+        <img src="logo.svg" className="logo" style={{ paddingLeft: "5px" }} />
         <div className="flex" style={{ justifyContent: "center" }}>
           <button>1</button>
           <button>2</button>
