daebfc33a603a882a5db24690e4ff88318cc8be0
aandrepingu <lungua0@gmail.com>
2023-10-06 16:58:43 -0400

    Added react-router-dom and attempting to use it (realized I am using it wrong but just committing cuz why not)

--- FoCSTools Site/src/main.tsx
+++ FoCSTools Site/src/main.tsx
@@ -1,10 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./routes/App.tsx";
 import "./index.css";
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import ErrorPage from "./errorPage.tsx";
+import Test from "./routes/Test.tsx";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        path: "/Home",
+        element: <Test/>
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
