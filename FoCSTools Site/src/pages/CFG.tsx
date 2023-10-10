920dd6a62915a5821f05ac63872be05ad026d036
Jordon1377 <43587817+Jordon1377@users.noreply.github.com>
2023-10-10 17:30:23 -0400

    CFG Remove-Update

--- FoCSTools Site/src/pages/CFG.tsx
+++ FoCSTools Site/src/pages/CFG.tsx
@@ -4,33 +4,44 @@ import "./CFG.css";
 export default function CFG() {
   const [count, setCount] = useState<number>(0);
   const [text, setText] = useState<string[]>([]);
+  const [currentTextIndex, setCurrentTextIndex] = useState<number>(count - 1);
 
-    function onAdd() {
-      setCount(count+1);
-      const newText = [...text, ''];
-      setText(newText);
+  function onTextClick(index: number) {
+    setCurrentTextIndex(index);
+    //console.log(currentTextIndex);
+  }
+
+  function onAdd() {
+    setCurrentTextIndex(count);
+    setCount(count + 1);
+    const newText = [...text, ""];
+    setText(newText);
+  }
+
+  function onRemove() {
+    if (count > 0) {
+      setCount(count - 1);
+      text.splice(currentTextIndex, 1);
+      setCurrentTextIndex(count - 2);
     }
 
-    function onRemove() {
-      if(count > 1){
-        setCount(count-1);
-      }
-      const newText = [...text];
-      newText.pop()
-      setText(newText);
-    }
+    //const newText = [...text];
+    //newText.pop();
+    //setText(newText);
+  }
 
-    function clear(){
-      const newText = [''];
-      setText(newText);
-      setCount(1);
-    }
+  function clear() {
+    const newText = [""];
+    setText(newText);
+    setCount(0);
+    setCurrentTextIndex(-1);
+  }
 
-    const onWrite = (index: number, value: string) => {
-        const newText = [...text];
-        newText[index] = value;
-        setText(newText);
-    }
+  const onWrite = (index: number, value: string) => {
+    const newText = [...text];
+    newText[index] = value;
+    setText(newText);
+  };
 
   return (
     <>
@@ -48,10 +59,12 @@ export default function CFG() {
               type="text"
               value={text[index]}
               onChange={(e) => onWrite(index, e.target.value)}
+              onClick={() => onTextClick(index)}
             />
           ))}
         </div>
         <h1>{text}</h1>
+        <h1>{currentTextIndex}</h1>
       </div>
     </>
   );
