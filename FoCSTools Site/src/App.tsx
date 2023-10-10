65cc9f6e30a0fedc7a24ef5a76348e19e87dbe5c
aandrepingu <lungua0@gmail.com>
2023-10-10 16:59:31 -0400

    Removed unused imports in App.tsx

--- FoCSTools Site/src/App.tsx
+++ FoCSTools Site/src/App.tsx
@@ -1,48 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
 import Home from "./pages/Home";
 import DFA from "./pages/DFA";
 import Landing from "./pages/Landing";
 import TuringMachine from "./pages/TuringMachine";
 import CFG from "./pages/CFG"
-import { useState, useEffect } from "react";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-//export type GraphType = "DFA" | "CFG" | "TU";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 export default function App() {
-  /*const [path, setPath] = useState("/Landing");
-  const [graphType, setGraphType] = useState<GraphType>("DFA");
-
-  const paths: { [id: string]: JSX.Element } = {
-    "/Test": <Test />,
-    "/DFA": <DFA />,
-    "/TuringMachine": <TuringMachine />,
-    "/Landing": <Landing setComponent={() => setPath("/Test")} />,
-  }
-  useEffect(() => {
-    const windowPath: string = window.location.pathname;
-    if (windowPath in paths) {
-      setPath(windowPath);
-    } else {
-      setPath("/Landing");
-    }
-  }, []);
-  */
-  /*const [path, setPath] = useState("/Landing");
-  const state: {[id: string]: JSX.Element} = {
-    "/Test": <Test />,
-    "/Landing": <Landing/>
-  }
-  useEffect(() => {
-    const windowPath: string = window.location.pathname;
-    if(windowPath in state){
-      setPath(windowPath);
-    } else { 
-      setPath("/Landing");
-    }
-  }, []);*/
-
   return (
     <div>
       <BrowserRouter>
