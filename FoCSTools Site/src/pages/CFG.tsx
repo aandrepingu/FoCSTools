d2a8f0586905b02ac45bce8c84481f9c5000738c
faizaan5ali <130408625+faizaan5ali@users.noreply.github.com>
2023-10-11 16:26:23 -0400

    Created method to output grammar contexts in divs,
    Before it was messily concatenated, the next step is to produce (10?) strings using the user's rules

--- FoCSTools Site/src/pages/CFG.tsx
+++ FoCSTools Site/src/pages/CFG.tsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
 import "./CFG.css";
 
+
 export default function CFG() {
   const [count, setCount] = useState<number>(0);
+  const [lang, setLang] = useState<string>("");
   const [text, setText] = useState<string[]>([]);
   const [currentTextIndex, setCurrentTextIndex] = useState<number>(count - 1);
 
+  // Get index of clicked text box
   function onTextClick(index: number) {
     setCurrentTextIndex(index);
-    //console.log(currentTextIndex);
   }
 
+  // Append to grammar array
   function onAdd() {
     setCurrentTextIndex(count);
     setCount(count + 1);
@@ -18,18 +21,27 @@ export default function CFG() {
     setText(newText);
   }
 
+  // Remove from grammar array
+    // Remove from back
+    // If a box is clicked, remove that one
   function onRemove() {
     if (count > 0) {
       setCount(count - 1);
       text.splice(currentTextIndex, 1);
       setCurrentTextIndex(count - 2);
     }
-
-    //const newText = [...text];
-    //newText.pop();
-    //setText(newText);
   }
 
+  // Output grammars in a div
+  const renderOutputs = () => {
+    const renderValues: JSX.Element[] = []
+    for(let i = 0; i < text.length; i++){
+      renderValues.push(<p key={i}>{ text[i] }</p>);
+    }
+    return renderValues;
+  };
+
+  // Clear all text boxes and array
   function clear() {
     const newText = [""];
     setText(newText);
@@ -37,20 +49,31 @@ export default function CFG() {
     setCurrentTextIndex(-1);
   }
 
+  // Edit the value in the array
   const onWrite = (index: number, value: string) => {
     const newText = [...text];
     newText[index] = value;
     setText(newText);
   }
+
+  // Handle special keypresses "|" and "Backspace" to add and remove
   const handleKeyPress=(e: React.KeyboardEvent<HTMLInputElement>)=>{
     const value=e.currentTarget.value;
     if(e.key==='|') onAdd();
     else if(e.key==='Backspace'&& !value)onRemove();
   }
+
+  // Edit a text box
   const handleChange=(index: number, e:React.FormEvent<HTMLInputElement>)=>{
     const value=e.currentTarget.value;
     if(value[value.length-1]==='|')return
     onWrite(index,value)
+  }
+
+  // Handle the name of the language being generated
+  const handleLang=(e:React.FormEvent<HTMLInputElement>)=>{
+    const value=e.currentTarget.value;
+    setLang(value)
   }
   return (
     <>
@@ -60,6 +83,11 @@ export default function CFG() {
           <button onClick={onAdd}>Add</button>
           <button onClick={onRemove}>Remove</button>
           <button onClick={clear}>Clear</button>
+          <input
+            type="text"
+            value={lang}
+            onChange={(e) => handleLang(e)}
+          />
         </div>
         <div className="CFG_Rules">
           {Array.from({ length: count }).map((_, index) => (
@@ -73,8 +101,11 @@ export default function CFG() {
             />
           ))}
         </div>
-        <h1>{text}</h1>
         <h1>{currentTextIndex}</h1>
+        <h1>{lang}</h1>
+      </div>
+      <div className="outputBox">
+        {renderOutputs()}
       </div>
     </>
   );
