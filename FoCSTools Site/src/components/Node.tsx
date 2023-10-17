fd4613b7f3607ac130085c3269070be20c0e42cb
Zain Magdon-Ismail <84550903+ZainMI@users.noreply.github.com>
2023-10-17 16:55:18 -0400

    name with underscore

--- FoCSTools Site/src/components/Node.tsx
+++ FoCSTools Site/src/components/Node.tsx
@@ -6,17 +6,47 @@ import { useState } from "react";
 export default function Node({ node }: { node: NodeType }) {
   const [name, setName] = useState("name");
   const [isEditing, setIsEditing] = useState(false);
+  const [beforeUnderscore, setBeforeUnderscore] = useState(name);
+  const [afterUnderscore, setAfterUnderscore] = useState("");
+
+  function SplitName(word: string) {
+    const nameSplit = word.split("_");
+    if (nameSplit.length === 2) {
+      setBeforeUnderscore(nameSplit[0]);
+      setAfterUnderscore(nameSplit[1]);
+    } else {
+      setBeforeUnderscore(word);
+      setAfterUnderscore("");
+    }
+  }
+
   return (
     <>
       <Draggable>
         <div>
-          {isEditing && <div className={"editBox"}>
-            Edit<br/>
-            <button className={"editButton"}>Change 0</button>
-            <button className={"editButton"}>Change 1</button><br/>
-            <button className={"editButton"}>Delete</button>
-            <button className={"editButton"}>Change Name</button>
-          </div>}
+          {isEditing && (
+            <div className={"editBox"}>
+              Name:
+              <br />
+              <input
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                  SplitName(e.target.value);
+                }}
+                style={{ width: "100px" }}
+              />
+              <br />
+              Edit:
+              <br />
+              <div className={"editButtons"}>
+                <button className={"editButton"}>Change 0</button>
+                <button className={"editButton"}>Change 1</button>
+                <br />
+                <button className={"editButton"}>Delete</button>
+              </div>
+            </div>
+          )}
           <div className={"node"} id={node.id}>
             <div
               style={{
@@ -31,8 +61,13 @@ export default function Node({ node }: { node: NodeType }) {
             >
               <div className={"edit"}>e</div>
             </div>
-
-            {name}
+            <div>
+              <span style={{ color: "white" }}>{beforeUnderscore}</span>
+              <span style={{ fontSize: "10px", color: "white" }}>
+                {" "}
+                {afterUnderscore}
+              </span>
+            </div>
           </div>
         </div>
       </Draggable>
