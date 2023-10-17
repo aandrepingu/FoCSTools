68f5769b641a81ebb2b0a644b47e97be77879640
aandrepingu <lungua0@gmail.com>
2023-10-17 16:47:06 -0400

    Added some buttons to Node edit menu to be used later

--- FoCSTools Site/src/components/Node.tsx
+++ FoCSTools Site/src/components/Node.tsx
@@ -6,12 +6,17 @@ import { useState } from "react";
 export default function Node({ node }: { node: NodeType }) {
   const [name, setName] = useState("name");
   const [isEditing, setIsEditing] = useState(false);
-
   return (
     <>
       <Draggable>
         <div>
-          {isEditing && <div className={"editBox"}>hi</div>}
+          {isEditing && <div className={"editBox"}>
+            Edit<br/>
+            <button className={"editButton"}>Change 0</button>
+            <button className={"editButton"}>Change 1</button><br/>
+            <button className={"editButton"}>Delete</button>
+            <button className={"editButton"}>Change Name</button>
+          </div>}
           <div className={"node"} id={node.id}>
             <div
               style={{
